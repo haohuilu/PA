b14ea352f9a8d5240b4bd46e9db8742a1fec74ab
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-02-02 15:02:12 +1100

    Add files via upload

--- models/SVC.py
+++ models/SVC.py
@@ -4,10 +4,9 @@ from sklearn.svm import SVC
 
 
 def svc_param_selector():
-    C = st.number_input("C", 10)
-    kernel = st.selectbox("kernel", ("rbf"))
-    gamma = st.selectbox("gamma", ("scale"))
+    C = st.number_input("C", 10, 2.0, 1.0, 0.01)
+    kernel = st.selectbox("kernel", ("rbf", "linear", "poly", "sigmoid"))
     random_state = 1
-    params = {"C": C, "kernel": kernel, "gamma" = gamma, "random_state": random_state}
+    params = {"C": C, "kernel": kernel, "random_state": random_state}
     model = SVC(**params)
     return model
