2b4d889deb4012e0882ab765bbbf7ddba1913866
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-02-02 14:30:32 +1100

    Add files via upload

--- utils/functions.py
+++ utils/functions.py
@@ -58,18 +58,7 @@ def plot_decision_boundary_and_metrics(
 
 
 
-    fig = make_subplots(
-        rows=2,
-        cols=2,
-        specs=[[{"colspan": 2}, None], [{"type": "indicator"}, {"type": "indicator"}]],
-        subplot_titles=("Model Performance", None, None),
-        row_heights=[0.1, 0.1],
-    )
-
-
-
-
-    fig.add_trace(
+    fig = 
         go.Indicator(
             mode="gauge+number+delta",
             value=metrics["test_accuracy"],
@@ -79,7 +68,7 @@ def plot_decision_boundary_and_metrics(
         ),
         row=2,
         col=1,
-    )
+    
 
     fig.add_trace(
         go.Indicator(
