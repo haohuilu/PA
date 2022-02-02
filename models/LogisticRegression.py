96cfcd6c0f81bdf0c93191389abc19be8b1fe26a
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-02-02 14:02:00 +1100

    Add files via upload

--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 def lr_param_selector():
 
     solver = st.selectbox(
-        "solver", options=["lbfgs", "newton-cg", "liblinear", "sag", "saga"]
+        "solver", options=["liblinear"]
     )
 
     if solver in ["newton-cg", "lbfgs", "sag"]:
@@ -16,14 +16,14 @@ def lr_param_selector():
         penalties = ["l1", "l2", "none", "elasticnet"]
 
     elif solver == "liblinear":
-        penalties = ["l1"]
+        penalties = ["l2"]
 
     penalty = st.selectbox("penalty", options=penalties)
-    C = st.number_input("C", 0.1, 2.0, 1.0, 0.1)
+    C = st.number_input("C", 100)
     C = np.round(C, 3)
-    max_iter = st.number_input("max_iter", 100, 2000, step=50, value=100)
+    #max_iter = st.number_input("max_iter", 100, 2000, step=50, value=100)
 
-    params = {"solver": solver, "penalty": penalty, "C": C, "max_iter": max_iter}
+    params = {"solver": solver, "penalty": penalty, "C": C, "max_iter": max_iter, "random_state": 1}
 
     model = LogisticRegression(**params)
     return model
