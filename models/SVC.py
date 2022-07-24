21a21b31222de98c5fbf95717939f4fdc1e09338
haohuilu <95997507+haohuilu@users.noreply.github.com>
2022-07-24 15:12:04 +1000

    Add files via upload

--- models/SVC.py
+++ models/SVC.py
@@ -4,7 +4,7 @@ from sklearn.svm import SVC
 
 
 def svc_param_selector():
-    C = st.number_input("C", 10)
+    C = st.number_input("C", 1)
     kernel = st.selectbox("kernel", ("rbf", "linear", "poly", "sigmoid"))
     params = {"C": C, "kernel": kernel}
     model = SVC(**params)
